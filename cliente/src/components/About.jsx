f7557f1c0a735ea8c17af7cedb611eacbd95690a
AquinoEdissonFacundo <124944928+AquinoEdissonFacundo@users.noreply.github.com>
2023-05-20 20:47:20 -0300

    actualizacion importante

---
+++ cliente/src/components/About.jsx
@@ -0,0 +1,9 @@
+const About=()=>{
+    return(
+    <div>
+        <h1></h1>
+        <p>Edisson Facundo</p>
+    </div>
+    )
+}
+export default About
--- src/components/About.jsx
+++
@@ -1,9 +0,0 @@
-const About=()=>{
-    return(
-    <div>
-        <h1>work</h1>
-        <p>Mi Life</p>
-    </div>
-    )
-}
-export default About
